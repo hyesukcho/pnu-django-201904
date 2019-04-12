6e39a9b63525a8312968cabcc190f8661e1aa9e3
Chinseok Lee <me@askcompany.kr>
2019-04-12 17:22:12 +0900

    ShopAdmin/ItemAdmin 커스텀

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Shop, Item
 
-admin.site.register(Shop)
 
-admin.site.register(Item)
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['id', 'name', 'name_length', 'address']
+    list_display_links = ['name']
+
+    def name_length(self, shop):
+        return '{}글자'.format(len(shop.name))
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_filter = ['created_at']
