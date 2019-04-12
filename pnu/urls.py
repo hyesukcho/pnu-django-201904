0fe9551f48dd0b1d390fbf810849fabd1cb0e1d6
Chinseok Lee <me@askcompany.kr>
2019-04-12 17:08:34 +0900

    / 주소에 대한 redirect 처리

--- pnu/urls.py
+++ pnu/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+# FIXME: 이 코드는 RedirectView에 의해서 제거될 것입니다.
+from django.shortcuts import redirect
+def root(request):
+    return redirect("/shop/")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('', root),
 ]
