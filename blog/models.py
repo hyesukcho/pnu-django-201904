4e35344dcf28cbc2de920fcc9972a75998e7903a
Chinseok Lee <me@askcompany.kr>
2019-05-10 17:04:41 +0900

    django-bootstrap4 적용

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
 
 
 class Post(models.Model):
     # author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(6)])
     content = models.TextField()
     ip = models.GenericIPAddressField()
     created_at = models.DateTimeField(auto_now_add=True)
