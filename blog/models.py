4dc6aa65008b628495cc98b4d6d0ebb33a81317c
Chinseok Lee <me@askcompany.kr>
2019-05-10 11:20:51 +0900

    Post 모델을 생성하고, 마이그레이션 및 admin 등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    ip = models.GenericIPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
