b880190a065b7d3ebd97e50abdc394135b886704
Chinseok Lee <me@askcompany.kr>
2019-05-10 15:40:48 +0900

    로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 # FIXME: accounts는 app_name을 쓰지 않습니다.
 # app_name = ''
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
