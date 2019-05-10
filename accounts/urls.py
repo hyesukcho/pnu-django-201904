bda95fb4454b6db8c182913b37e60ba459e30726
Chinseok Lee <me@askcompany.kr>
2019-05-10 17:19:13 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(form_class=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('signup/', views.signup, name='signup'),
 ]
